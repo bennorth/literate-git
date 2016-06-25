a0abea2759eb2836dd6b7b1f09041d39710fc3da
Ben North <ben@redfrontdoor.org>
2016-06-25 08:15:25 +0100

    _commit(): Add

---
+++ literategit/__init__.py
@@ -0,0 +1,13 @@
+import pygit2 as git
+
+
+def _commit(repo, oid, required_n_parents=None, tag=None):
+    commit = repo[oid]
+    if not isinstance(commit, git.Commit):
+        raise ValueError('not a Commit')
+    parent_ids = commit.parent_ids
+    n_parents = len(parent_ids)
+    if required_n_parents is not None and n_parents != required_n_parents:
+        raise ValueError('commit {} has {} parent/s so is not a {}'
+                         .format(oid, n_parents, tag))
+    return commit
