e219ac57cce5ec22b74e05016497590265ffebcd
Ben North <ben@redfrontdoor.org>
2016-07-26 21:46:07 +0100

    Update JS to fade in/out diff/children
    Sliding seemed a bit much now it's a swap.

--- literategit/literate-git.js
+++ literategit/literate-git.js
@@ -1,21 +1,18 @@
 $(document).ready(function() {
-    $('.expand-collapse > p > button').click(function(e) {
+    $('.diff-or-children > .nav').click(function(e) {
         var button = $(e.target);
         var node_elt = $(e.target).parents('.literate-git-node')[0];
-        var children_elt = $(node_elt).children('.children')[0];
+        var expanded_hdr = $(node_elt).find('.nav.collapse');
+        var collapsed_hdr = $(node_elt).find('.nav.expand');
+        var diff_elt = $(node_elt).find('.diff')[0];
+        var children_elt = $(node_elt).find('.children')[0];
         var children_visible_p = $(children_elt).is(':visible');
         if (children_visible_p) {
-            button.attr('disabled', true);
-            $(children_elt).slideUp(800, function() {
-                button.html('Show details');
-                button.attr('disabled', false);
-            });
+            expanded_hdr.fadeOut(250, function() { collapsed_hdr.fadeIn(250); });
+            $(children_elt).fadeOut(250, function() { $(diff_elt).fadeIn(250); });
         } else {
-            button.attr('disabled', true);
-            $(children_elt).slideDown(800, function() {
-                button.html('Hide details');
-                button.attr('disabled', false);
-            });
+            collapsed_hdr.fadeOut(250, function() { expanded_hdr.fadeIn(250); });
+            $(diff_elt).fadeOut(250, function() { $(children_elt).fadeIn(250); });
         }
     });
 });
