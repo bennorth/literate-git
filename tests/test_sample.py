a9e39979f30594c24cabca00541ca0222774abf1
Ben North <ben@redfrontdoor.org>
2016-08-12 21:42:57 +0100

    TestTamagotchi.test_render(): Add
    Monolithic test, so not great, but better than nothing.

--- tests/test_sample.py
+++ tests/test_sample.py
@@ -1,5 +1,10 @@
+import bs4
 import pygit2 as git
 import pytest
+
+import literategit.cli
+import literategit.dump_all_trees
+
 
 tamagotchi_github_url = 'https://github.com/bennorth/webapp-tamagotchi.git'
 
@@ -12,3 +17,28 @@ def tamagotchi_repo(tmpdir_factory):
     commit = repo[branch.target]
     repo.create_branch('start', commit)
     return repo
+
+
+class TestTamagotchi:
+    def test_render(self, tamagotchi_repo):
+        """
+        This is fragile in that it relies on the exact state of the 'Tamagotchi'-style
+        webapp repo, but it does at least check all the parts fit together.
+        """
+        args = ['start', 'for-rendering', 'sample_create_url.CreateUrl']
+        output_list = []
+        literategit.cli.render(_argv=args,
+                               _path=tamagotchi_repo.path,
+                               _print=output_list.append)
+
+        soup = bs4.BeautifulSoup(output_list[0], 'html.parser')
+        node_divs = soup.find_all('div', class_='literate-git-node')
+        got_sha1s = sorted(d.attrs['data-commit-sha1'] for d in node_divs)
+
+        exp_commits = literategit.dump_all_trees.collect_commits(tamagotchi_repo,
+                                                                 'start',
+                                                                 'for-rendering')
+        exp_sha1s = sorted(c.hex for c in exp_commits)
+
+        assert got_sha1s == exp_sha1s
+        assert len(got_sha1s) == 162  # More fragility
