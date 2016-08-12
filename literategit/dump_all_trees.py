b6c7059aa9ad0370372f808d1ea49f7c661b7489
Ben North <ben@redfrontdoor.org>
2016-08-12 19:35:02 +0100

    mkdir_excl(): Add

--- literategit/dump_all_trees.py
+++ literategit/dump_all_trees.py
@@ -1,3 +1,6 @@
+import os
+
+
 def collect_commits(repo, rev1, rev2):
     """
     List of all Commit objects which are reachable from rev2 but not
@@ -7,3 +10,10 @@ def collect_commits(repo, rev1, rev2):
     walker = repo.walk(repo.revparse_single(rev2).id)
     walker.hide(repo.revparse_single(rev1).id)
     return list(walker)
+
+
+def mkdir_excl(dirname):
+    if os.path.exists(dirname):
+        raise ValueError('directory "{}" already exists'
+                         .format(dirname))
+    os.makedirs(dirname)
