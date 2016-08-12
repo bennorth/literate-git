214d82a3419d59382fedc838934c08f94615cc29
Ben North <ben@redfrontdoor.org>
2016-08-12 19:34:25 +0100

    collect_commits(): Add
    In new 'dump_all_trees' module.

---
+++ literategit/dump_all_trees.py
@@ -0,0 +1,9 @@
+def collect_commits(repo, rev1, rev2):
+    """
+    List of all Commit objects which are reachable from rev2 but not
+    reachable from rev1.  Commits are not returned in any particular
+    order.
+    """
+    walker = repo.walk(repo.revparse_single(rev2).id)
+    walker.hide(repo.revparse_single(rev1).id)
+    return list(walker)
