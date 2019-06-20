988a2eed60ab7df189220b53e70b22443afec373
Ben North <ben@redfrontdoor.org>
2019-06-20 21:37:24 +0100

    Test

--- tests/test_sample.py
+++ tests/test_sample.py
@@ -50,6 +50,7 @@ class TestLocalRepo:
         assert len(output_list) == 1
         output_text = output_list[0]
         assert "Add documentation" in output_text
+        assert "Add <code>colours</code> submodule" in output_text
 
 
 @pytest.fixture(scope='session')
