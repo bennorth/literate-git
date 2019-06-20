de509153d20f015cfe7b927c142c7963929504be
Ben North <ben@redfrontdoor.org>
2019-06-20 16:08:56 +0100

    Remove now-unused testing create-url module

--- tests/sample_create_url.py
+++
@@ -1,8 +0,0 @@
-class CreateUrl:
-    @staticmethod
-    def result_url(sha1):
-        return 'http://example.com/results/{}'.format(sha1)
-
-    @staticmethod
-    def source_url(sha1):
-        return 'http://example.com/source/{}'.format(sha1)
