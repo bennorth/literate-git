87527c715ac55031651b4b96310205403ae39fc4
Ben North <ben@redfrontdoor.org>
2020-02-15 07:03:41 +0000

    Test support: Create URL with query params

--- literategit/example_create_url.py
+++ literategit/example_create_url.py
@@ -6,3 +6,13 @@ class CreateUrl:
     @staticmethod
     def source_url(sha1):
         return 'http://example.com/source/{}'.format(sha1)
+
+
+class CreateQueryUrl:
+    @staticmethod
+    def result_url(sha1):
+        return 'http://example.com/results/?colour=blue&sha1={}'.format(sha1)
+
+    @staticmethod
+    def source_url(sha1):
+        return 'http://example.com/source/{}'.format(sha1)
